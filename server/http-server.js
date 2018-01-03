be270fb4f42a36195988a03620169dab7d179b02
Jan Korona <jan.korona@blstream.com>
2018-01-03 17:25:03 +0100

    stub simple api

--- server/http-server.js
+++ server/http-server.js
@@ -10,11 +10,27 @@ module.exports = {
     config = Object.assign({}, DEFAULT_CONFIG, config);
 
     const server = http.createServer((request, response) => {
-      fs.readFile('client/index.html', function (err, data) {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(data);
-        response.end();
-      });
+      console.log(`Retrieved request to url ${request.url}...`);
+      switch (request.url) {
+        case '/routes':
+          response.writeHead('200', { 'Content-Type': 'application/json' })
+          response.write(JSON.stringify([ 
+            { id: '001', label: 'Foo' },
+            { id: '002', label: 'Bar' },
+           ]));
+           response.end();
+          break;
+        case '/':
+          fs.readFile('client/index.html', function (err, data) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(data);
+            response.end();
+          });
+          break;
+        // default:
+        //   response.writeHead(301, { 'Content-Type': 'text/html', 'Location': '/' });
+        //   response.end();
+      }
     }).listen(config.port);
 
     console.log(`Http Server started on port ${config.port}...`);

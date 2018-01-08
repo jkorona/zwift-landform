f01caffa50747c930c189f9bd5cfac70bdb1c6b6
Jan Korona <jan.korona@blstream.com>
2018-01-08 14:05:13 +0100

    refactor HttpServer to class

--- server/http.server.js
+++ server/http.server.js
@@ -1,60 +1,68 @@
-const http = require('http');
-const fs = require('fs');
+class HttpServer {
 
-const DEFAULT_CONFIG = {
-  port: 3000
-};
-
-function callApi(handler, response) {
-  Promise.resolve(handler())
-    .then((data) => {
-      response.writeHead('200', { 'Content-Type': 'application/json' })
-      response.write(JSON.stringify(data));
-      response.end();
-    });
-}
-
-function redirectToIndex(response) {
-  response.writeHead(301, { 'Content-Type': 'text/html', 'Location': '/' });
-  response.end();
-}
-
-function sendIndexHtml(response) {
-  fs.readFile('client/index.html', function (err, data) {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
-  });
-}
-
-function createHttpHandler(resourceHandlers) {
-  return function httpHandler(request, response) {
-    console.log(`Retrieved request to url ${request.url}...`);
-
-    const resourceName = request.url.substr(1);
-
-    if (resourceName) {
-      if (resourceHandlers.hasOwnProperty(resourceName)) {
-        callApi(resourceHandlers[resourceName], response);
-      } else {
-        redirectToIndex(response);
-      }
-    } else {
-      sendIndexHtml(response);
-    }
+  static get DEFAULT_CONFIG() {
+    return {
+      port: 3000
+    };
   };
-}
 
-module.exports = {
+  constructor(http, fs) {
+    this.http = http;
+    this.fs = fs;
+  }
+
   start(resourceHandlers = {}, config = {}) {
-    config = Object.assign({}, DEFAULT_CONFIG, config);
+    config = Object.assign({}, HttpServer.DEFAULT_CONFIG, config);
 
-    const server = http
-      .createServer(createHttpHandler(resourceHandlers))
+    const server = this.http
+      .createServer(this.createHttpHandler(resourceHandlers))
       .listen(config.port);
 
     console.log(`Http Server started on port ${config.port}...`);
 
     return server;
   }
-};
+
+  createHttpHandler(resourceHandlers) {
+    return (request, response) => {
+      console.log(`Retrieved request to url ${request.url}...`);
+
+      const resourceName = request.url.substr(1);
+
+      if (resourceName) {
+        if (resourceHandlers.hasOwnProperty(resourceName)) {
+          this.callApi(resourceHandlers[resourceName], response);
+        } else {
+          this.redirectToIndex(response);
+        }
+      } else {
+        this.sendIndexHtml(response);
+      }
+    };
+  }
+
+  callApi(handler, response) {
+    Promise.resolve(handler())
+      .then((data) => {
+        response.writeHead('200', { 'Content-Type': 'application/json' })
+        response.write(JSON.stringify(data));
+        response.end();
+      });
+  }
+
+  redirectToIndex(response) {
+    response.writeHead(301, { 'Content-Type': 'text/html', 'Location': '/' });
+    response.end();
+  }
+
+  sendIndexHtml(response) {
+    this.fs.readFile('client/index.html', function (err, data) {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(data);
+      response.end();
+    });
+  }
+
+}
+
+module.exports = HttpServer;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-const httpServer = require('./http.server');
+const http = require('http');
+const fs = require('fs');
+
+const HttpServer = require('./http.server');
 
 const DataSource = require('./storage/data-source');
 const MockStorage = require('./storage/mock.store');
@@ -7,6 +10,8 @@ const dataSource = new DataSource(new MockStorage());
 
 module.exports = {
   boot() {
+    const httpServer = new HttpServer(http, fs);
+
     httpServer.start({
       route() {
         return dataSource.getRoute('12136784')

0f0e38af6f93bd880ceb036b5342224f02c6a27a
Jan Korona <jan.korona@blstream.com>
2018-02-20 12:36:35 +0100

    move client code to correct folder

---
+++ client/test/unit/specs/HelloWorld.spec.js
@@ -0,0 +1,14 @@
+// import Vue from 'vue'
+// import AthleteTracker from '@/components/AthleteTracker'
+
+describe('AthleteTracker', () => {
+  // it('should render correct contents', () => {
+  //   const Constructor = Vue.extend(AthleteTracker)
+  //   const vm = new Constructor().$mount()
+  //   expect(vm.$el.querySelector('.hello h1').textContent)
+  //     .toEqual('Welcome to Your Vue.js App')
+  // })
+  it('should be ok', () => {
+    expect(true).toEqual(true)
+  })
+})

65eade6fa198ccd6148666ebd073fca1deeb6326
Jan Korona <jan.korona@blstream.com>
2018-02-27 15:40:22 +0100

    ModuleLoader implementation

---
+++ server/connection/http/util/module-loader.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const glob = require('glob');
+
+const DEFAULT_CONFIG = {
+  dir: '/src',
+  cwd: process.cwd(),
+  pattern: '**/*.js'
+};
+
+class ModuleLoader {
+
+  static load(config) {
+    const { cwd, pattern, dir } = { ...DEFAULT_CONFIG, ...config };
+
+    const files = glob.sync(path.join(dir, pattern), { cwd });
+
+    return files.map((fileName) => require(path.join(cwd, fileName)));
+  }
+
+}
+
+module.exports = ModuleLoader;
---
+++ test/server/connection/http/util/module-loader.spec.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai');
+
+const ModuleLoader = require('../../../../../server/connection/http/util/module-loader');
+const MockController = require('../mock/mock.controller');
+
+describe('ModuleLoader', () => {
+
+  it('should load class from given folder', () => {
+    // given
+    const config = { dir: 'test/server', pattern: '**/*.controller.js' };
+    
+    // when
+    const classes = ModuleLoader.load(config);
+
+    // then
+    expect(classes).to.have.lengthOf(1);
+    expect(classes[0]).to.equal(MockController);
+  });
+
+  it('should return empty array if pattern does not match any file', () => {
+    // given
+    const config = { dir: 'test/server', pattern: '**/*.nope.js' };
+    
+    // when
+    const classes = ModuleLoader.load(config);
+
+    // then
+    expect(classes).to.be.empty;
+  });
+
+});

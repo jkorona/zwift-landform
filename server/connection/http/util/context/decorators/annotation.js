2fe0fcec833b003891e2315bab27538d5cb73415
Jan Korona <jan.korona@blstream.com>
2018-03-13 13:30:59 +0100

    annotation base class

---
+++ server/connection/http/util/context/decorators/annotation.js
@@ -0,0 +1,22 @@
+
+function ClassAnnotation(fn) {
+  function annotation(...params) {
+    return (target) => {
+      const ctx = { class: target, prototype: target.prototype };
+
+      annotation.$$processors.forEach((processor) => processor(target));
+      fn.apply(ctx, params);
+    }
+  }
+  annotation.$$processors = [];
+  annotation.extends = function (base) {
+    annotation.$$processors.push(base);
+
+    return annotation;
+  }
+  return annotation;
+}
+
+module.exports = {
+  ClassAnnotation
+}
---
+++ test/server/connection/http/util/context/decorators/annotation.spec.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const { spy } = require('sinon');
+
+import { ClassAnnotation } from '../../../../../../../server/connection/http/util/context/decorators/annotation';
+
+describe.only('annotation', () => {
+
+  describe('classAnnotation', () => {
+
+    it('should execute simple annotation on class', () => {
+      // given
+      const fn = spy();
+      const Simple = ClassAnnotation(fn);
+
+      // when
+      @Simple('foo', 'bar')
+      class T { };
+
+      // then
+      expect(fn.calledOnce).to.be.true;
+      expect(fn.lastCall.args).to.eql(['foo', 'bar']);
+      expect(fn.lastCall.thisValue).to.eql({ class: T, prototype: T.prototype });
+    });
+
+    it('should execute complex annotation on class', () => {
+      // given
+      const fn1 = spy();
+      const fn2 = spy();
+
+      const Simple = ClassAnnotation(fn1);
+      const Complex = ClassAnnotation(fn2).extends(Simple('foo'));
+
+      // when
+      @Complex('bar')
+      class T { };
+
+      // then
+      expect(fn1.calledOnce).to.be.true;
+      expect(fn1.lastCall.args).to.eql(['foo']);
+      expect(fn1.lastCall.thisValue).to.eql({ class: T, prototype: T.prototype });
+
+      expect(fn2.calledOnce).to.be.true;
+      expect(fn2.lastCall.args).to.eql(['bar']);
+      expect(fn2.lastCall.thisValue).to.eql({ class: T, prototype: T.prototype });
+
+      expect(fn1.calledBefore(fn2)).to.be.true;
+    });
+
+  });
+
+});
